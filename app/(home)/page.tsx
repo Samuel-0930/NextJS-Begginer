cd5ec0f75b440289a652cb3c2336d4ea597637fe
Samyeol Son <samyeol0930@gmail.com>
2024-05-17 12:42:39 +0900

    [Setup] 프로젝트 시작 전 준비

---
+++ app/(home)/page.tsx
@@ -0,0 +1,28 @@
+import { Metadata } from 'next';
+import Link from 'next/link';
+
+export const metadata: Metadata = {
+	title: 'Home',
+};
+
+export const API_URL = 'https://nomad-movies.nomadcoders.workers.dev/movies';
+
+const getMovies = async () => {
+	// await new Promise((resolve) => setTimeout(resolve, 1000));
+	const res = await fetch(API_URL);
+	const json = await res.json();
+	return json;
+};
+
+export default async function HomePage() {
+	const movies = await getMovies();
+	return (
+		<div>
+			{movies.map((movie) => (
+				<li key={movie.id}>
+					<Link href={`/movies/${movie.id}`}>{movie.title}</Link>
+				</li>
+			))}
+		</div>
+	);
+}
---
+++ app/about-us/page.tsx
@@ -0,0 +1,11 @@
+export const metadata = {
+	title: 'About us',
+};
+
+export default function AbuotUs() {
+	return (
+		<div>
+			<h1>About us!</h1>
+		</div>
+	);
+}
